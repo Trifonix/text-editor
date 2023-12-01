bfddd4659620d0c060ad951b4e5e8b0884bdbe14
Trifonix <trifonix@rocketmail.com>
2023-12-01 12:38:39 -0500

    add RawMode func (NO ECHO)

--- text-editor.c
+++ text-editor.c
@@ -5,12 +5,24 @@
  */
 
 /* Заголовочные файлы */
+#include <termios.h>
 #include <unistd.h>
+
+/* Активация "грубого" режима - отключение вывода введенных символов в консоль */
+void enableRawMode(void)
+{
+	struct termios raw;
+	tcgetattr(STDIN_FILENO, &raw);
+	raw.c_lflag &= ~(ECHO);
+	tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
+}
 
 /* Функция (точка) входа в программу */
 int main(void)
 {
-	/* Активация "грубого" режима - чтение по 1 байту из стандартного потока ввода */
+	enableRawMode();
+
+	/* Чтение по 1 байту из стандартного потока ввода */
 	/* При появлении символа 'e' и передаче ввода в программу клавишей 'Enter' цикл прерывается */
 	char c;
 	while (read(STDIN_FILENO, &c, 1) == 1 && c != 'e');
