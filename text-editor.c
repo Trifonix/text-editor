77216204b26c8fe59409016c7562cccc808cbd9c
Trifonix <trifonix@rocketmail.com>
2023-12-04 12:49:35 -0500

    add disableRawMode()

--- text-editor.c
+++ text-editor.c
@@ -5,15 +5,26 @@
  */
 
 /* Заголовочные файлы */
+#include <stdlib.h>
 #include <termios.h>
 #include <unistd.h>
+
+struct termios orig_termios;
+
+void disableRawMode()
+{
+	tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios);
+}
 
 /* Активация "грубого" режима - отключение вывода введенных символов в консоль */
 void enableRawMode(void)
 {
-	struct termios raw;
-	tcgetattr(STDIN_FILENO, &raw);
+	tcgetattr(STDIN_FILENO, &orig_termios);
+	atexit(disableRawMode);
+
+	struct termios raw = orig_termios;
 	raw.c_lflag &= ~(ECHO);
+
 	tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
 }
 
